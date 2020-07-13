03012f9387345256240b3ac29060b6b7214016c6
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-13 22:27:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,11 @@
 def my_collect(array)
   i=0
-  new_array=[]
+  collection=[]
   while i<array.length
-  yield (array[i])
+  collection << yield(array[i])
   i+=1
 end
+
 end
 #collection = ['ruby', 'javascript', 'python', 'objective-c']
 #my_collect(collection) do |lang| lang.upcase end
