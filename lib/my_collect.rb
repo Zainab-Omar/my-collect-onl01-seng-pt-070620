3ee79a45244fd8e9fe5c791d1cd2077c1412136c
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-13 22:26:34 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,12 @@
 def my_collect(array)
   i=0
-  #new_array=[]
+  new_array=[]
   while i<array.length
   yield (array[i])
   i+=1
 end
 end
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang| lang.upcase end
+#collection = ['ruby', 'javascript', 'python', 'objective-c']
+#my_collect(collection) do |lang| lang.upcase end
 
 
